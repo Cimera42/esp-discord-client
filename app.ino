2f503bc7631ec785aaa6f410fad5c4439af7764b
Tim Porritt <tim.m.porritt@gmail.com>
2019-09-30 23:31:06 +1000

    Format with spaces for indenting

--- app.ino
+++ app.ino
@@ -46,63 +46,72 @@ const char *host = "discordapp.com";
 const int httpsPort = 443;  //HTTPS= 443 and HTTP = 80
 void loop()
 {
-// Generic HTTPS client for sending messages at some point
+    // Generic HTTPS client for sending messages at some point
 
-// WiFiClientSecure httpsClient;                              //Declare an object of class HTTPClient
+    // WiFiClientSecure httpsClient;
 
-// Serial.print("HTTPS Connecting");
-//   int r=0; //retry counter
-//   while((!httpsClient.connect(host, httpsPort)) && (r < 30)){
-//       delay(100);
-//       Serial.print(".");
-//       r++;
-//   }
-//   if(r==30) {
-//     Serial.println("Connection failed");
-//   }
-//   else {
-//     Serial.println("Connected to web");
-//   }
-//   httpsClient.print(String("GET ") + "https://discordapp.com/api/gateway" + " HTTP/1.1\r\n" +
-//                "Host: " + host + "\r\n" +
-//                "Authorization: " + bot_token + "\r\n" +
-//                "Connection: close\r\n\r\n");
-// Serial.println("request sent");
+    // Serial.print("HTTPS Connecting");
+    // int r=0; //retry counter
+    // while((!httpsClient.connect(host, httpsPort)) && (r < 30))
+    // {
+    //     delay(100);
+    //     Serial.print(".");
+    //     r++;
+    // }
+    // if(r==30)
+    // {
+    //     Serial.println("Connection failed");
+    // }
+    // else
+    // {
+    //     Serial.println("Connected to web");
+    // }
+    // httpsClient.print(String("GET ") + "https://discordapp.com/api/gateway" + " HTTP/1.1\r\n" +
+    //             "Host: " + host + "\r\n" +
+    //             "Authorization: " + bot_token + "\r\n" +
+    //             "Connection: close\r\n\r\n");
+    // Serial.println("request sent");
 
-//   while (httpsClient.connected()) {
-//     String line = httpsClient.readStringUntil('\n');
-//     if (line == "\r") {
-//       Serial.println("headers received");
-//       break;
-//     }
-//   }
-//   Serial.println("reply was:");
-//   Serial.println("==========");
-//   String line;
-//   while(httpsClient.available()){
-//     line = httpsClient.readStringUntil('\n');  //Read Line by Line
-//     Serial.println(line); //Print response
-//   }
-//   Serial.println("==========");
-//   Serial.println("closing connection");
+    // while (httpsClient.connected())
+    // {
+    //     String line = httpsClient.readStringUntil('\n');
+    //     if (line == "\r")
+    //     {
+    //         Serial.println("headers received");
+    //         break;
+    //     }
+    // }
+    // Serial.println("reply was:");
+    // Serial.println("==========");
+    // String line;
+    // while(httpsClient.available())
+    // {
+    //     line = httpsClient.readStringUntil('\n');  //Read Line by Line
+    //     Serial.println(line); //Print response
+    // }
+    // Serial.println("==========");
+    // Serial.println("closing connection");
 
-//   delay(2000);  //GET Data at every 2 seconds
+    // delay(2000);  //GET Data at every 2 seconds
 
 
-	if (!ws.isConnected()) {
+    if (!ws.isConnected())
+    {
         Serial.println("connecting");
         // It technically should fetch url from discordapp.com/api/gateway
-		ws.connect("gateway.discord.gg", "https://gateway.discord.gg/", 443);
-	} else {
-		String msg;
-		if (ws.getMessage(msg)) {
-			Serial.println(msg);
+        ws.connect("gateway.discord.gg", "https://gateway.discord.gg/", 443);
+    } else {
+        String msg;
+        if (ws.getMessage(msg))
+        {
+            Serial.println(msg);
 
             // TODO Should maintain heartbeat
-            if(msg.indexOf("\"op\":10") != -1) {
+            if(msg.indexOf("\"op\":10") != -1)
+            {
                 ws.send("{\"op\":2,\"d\":{\"token\":\"" + bot_token + "\",\"properties\":{\"$os\":\"linux\",\"$browser\":\"ESP8266\",\"$device\":\"ESP8266\"},\"compress\":false,\"large_threshold\":250}}");
             }
-		}
-	}
-	delay(500);
+        }
+    }
+    delay(500);
 }
