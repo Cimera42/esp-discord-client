890569a84ea447c13c7a350b191d82652c267bdf
Tim Porritt <tim.m.porritt@gmail.com>
2022-08-19 00:15:56 +1000

    GitHub action to auto-renew Discord SSL fingerprint (#19)
    * GitHub action load gateway.discord.gg fingerprint
    
    * Add fingerprint marker in config
    
    * Replace fingerprint, create branch and commit
    
    * Git user config
    
    * Create PR

--- config.h
+++ config.h
@@ -9,6 +9,7 @@
 const uint16_t gateway_intents = GUILD_MESSAGES_INTENT | GUILD_MESSAGE_TYPING_INTENT;
 
 // discord.gg certificate fingerprint
-const char * certificateFingerprint = "8e fb bb d8 f0 69 9d 5b fe 30 ee bd 23 e0 7a 5b 56 41 86 e2";
+// <<FINGERPRINT>> are for automated fingeprint updating with GitHub actions
+const char * certificateFingerprint = /*FINGERPRINT>>*/"8e fb bb d8 f0 69 9d 5b fe 30 ee bd 23 e0 7a 5b 56 41 86 e2"/*<<FINGERPRINT*/;
 
 #endif //CONFIG_H
